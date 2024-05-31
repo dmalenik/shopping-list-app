5c52dc45dd5b6da3899e23404e8600168aae4034
Dmytro <dmalenik@gmail.com>
2024-05-31 15:22:02 +0300

    refactor(register): add some style changes
    Add spaces, change places of variables

--- api/register.py
+++ api/register.py
@@ -1,29 +1,32 @@
 from os import environ
 
-from dotenv import load_dotenv
+import psycopg2
+import psycopg2.extras
 
-import psycopg2 
-from psycopg2 import extras
+from dotenv import load_dotenv
 
 load_dotenv()
 
 db = f"dbname={environ["DATABASE"]} host={environ["DATABASE_HOST"]} user={environ["DATABASE_USER"]} password={environ["DATABASE_PASSWORD"]} port={environ["DATABASE_PORT"]}"
+db_table = "testing_users"
 
-
+# Register a new user
 def register(user):
-    with psycopg2.connect(db, cursor_factory=extras.DictCursor) as conn:
-        with conn.cursor() as curs:
-            db_table = "testing_users"
+    with psycopg2.connect(db) as conn:
+        with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as curs:
 
             # Check if user is already registered
             curs.execute(f"SELECT * FROM {db_table} WHERE username = %s AND email = %s;", (user["username"], user["email"]))
             
             if not curs.fetchone():
-                curs.execute(f"INSERT INTO {db_table} (username, email, hash) VALUES (%s, %s, %s);", (user['username'], user['email'], user['hash']))
+                curs.execute(f"INSERT INTO {db_table} (username, email, hash) VALUES (%s, %s, %s);", (user["username"], user["email"], user["hash"]))
             
+
             # For tests
             curs.execute(f"SELECT * FROM {db_table} WHERE username = %s AND email = %s;", (user["username"], user["email"]))
             return dict(curs.fetchone())
+
+
 # Edit user credentials
 def edit_user_data(user, edit_user):
     with psycopg2.connect(db) as conn:
