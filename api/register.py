8447b4892162c9d7257318dc720ae5279f409363
Dmytro <dmalenik@gmail.com>
2024-06-01 16:13:56 +0300

    feat(register): add function that removes user data from DB
    Write tests. Create function with a query to DB.

--- api/register.py
+++ api/register.py
@@ -14,9 +14,8 @@ db_table = "testing_users"
 def register(user):
     with psycopg2.connect(db) as conn:
         with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as curs:
-            curs.execute(f"SELECT * FROM {db_table} WHERE username = %s AND email = %s;", (user["username"], user["email"]))
-            
             # Check if user is already registered
+            curs.execute(f"SELECT * FROM {db_table} WHERE username = %s AND email = %s;", (user["username"], user["email"]))
             if not curs.fetchone():
                 curs.execute(f"INSERT INTO {db_table} (username, email, hash) VALUES (%s, %s, %s);", (user["username"], user["email"], user["hash"]))
             
@@ -43,6 +42,17 @@ def edit_user_data(user, edit_user):
             
             # For tests
             curs.execute(f"SELECT * FROM {db_table} WHERE username = %s AND email = %s;", (edit_user["username"], edit_user["email"]))
+            res = dict(curs.fetchone())
+
+    return res
+
+
+# Delete user credentials
+def delete_user_data(user):
+    with psycopg2.connect(db) as conn:
+        with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as curs:
+            curs.execute(f"DELETE FROM {db_table} WHERE username = %s AND email = %s RETURNING id, username, email, hash", (user["username"], user["email"]))
+            # For tests
             res = dict(curs.fetchone())
 
     return res
