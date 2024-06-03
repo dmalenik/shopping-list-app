44faef53c6e3da9a3443ef2e72dadd941cd4b30b
Dmytro <dmalenik@gmail.com>
2024-06-03 19:18:27 +0300

    feat(edit_dish, test_edit_dish): create a function to edit dish data
    Create testing case for edit dish data function. Create testing data for
    the function.
    Write a function to edit dish data.

--- api/handle_dishes.py
+++ api/handle_dishes.py
@@ -28,3 +28,27 @@ def add_dish(dish):
     
     return res
 
+
+# Update dish data
+def edit_dish(dish, edit_dish):
+    with psycopg2.connect(db) as conn:
+        with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as curs:
+            # Put edit_dish data into the DB
+            if edit_dish["name"]:
+                curs.execute(f"UPDATE {db_table} SET name = %s WHERE name = %s AND userid = %s", (edit_dish["name"], dish["name"], dish["userid"]))
+
+            if edit_dish["components"]:
+                # Convert components key to JSON
+                list_to_json_components = [json.dumps(component) for component in edit_dish["components"]]
+                curs.execute(f"UPDATE {db_table} SET components = %s WHERE name = %s AND userid = %s", (list_to_json_components, edit_dish["name"], dish["userid"]))
+
+            # For tests
+            curs.execute(f"SELECT name, components, userid FROM {db_table}")
+            res = dict(curs.fetchone())
+            # Decode JSON data
+            res["components"] = [json.loads(component) for component in res["components"]]
+            print(res)
+
+    return res
+
+
--- api/tests/test_dishes.py
+++ api/tests/test_dishes.py
@@ -2,7 +2,7 @@ import sys
 import os
 
 sys.path.append(os.path.abspath("/shopping-list-app/api/"))
-from handle_dishes import add_dish
+from handle_dishes import add_dish, edit_dish
 
 # Create class with tests
 class TestDishes:
@@ -17,9 +17,22 @@ class TestDishes:
         "userid": 59
     }
 
+    edit_dish = {
+        "name": "dish2",
+        "components": [
+            {"name": "component11", "unit": "kg", "size": 100500}, 
+            {"name": "component22", "unit": "gr", "size": 999}, 
+            {"name": "component33", "unit": "pieces", "size": 777}
+        ],
+        "userid": 59
+    }
+
 
     # Define test cases
     def test_add_dish(self):
         assert add_dish(self.dish) == self.dish
+
+    def test_edit_dish(self):
+        assert edit_dish(self.dish, self.edit_dish) == self.edit_dish
 
 
