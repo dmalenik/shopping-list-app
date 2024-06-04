d76f3f5a09b8e9338ea3420963be797af93092d0
Dmytro <dmalenik@gmail.com>
2024-06-04 10:42:01 +0300

    refactor(handle_dishes): rename key name to dish

--- api/handle_dishes.py
+++ api/handle_dishes.py
@@ -18,10 +18,10 @@ def add_dish(dish):
         with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as curs:
             # Adapt objects
             list_to_json_components = [json.dumps(component) for component in dish["components"]]
-            curs.execute(f"INSERT INTO {db_table}(name, components, userid) VALUES (%s, %s, %s);", (dish["name"], list_to_json_components, dish["userid"]))
+            curs.execute(f"INSERT INTO {db_table}(dish, components, userid) VALUES (%s, %s, %s);", (dish["dish"], list_to_json_components, dish["userid"]))
 
             # For tests
-            curs.execute(f"SELECT name, components, userid FROM {db_table};")
+            curs.execute(f"SELECT dish, components, userid FROM {db_table};")
             res = dict(curs.fetchone())
             # Decode JSON
             res["components"] = [json.loads(component) for component in res["components"]]
@@ -34,20 +34,19 @@ def edit_dish(dish, edit_dish):
     with psycopg2.connect(db) as conn:
         with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as curs:
             # Put edit_dish data into the DB
-            if edit_dish["name"]:
-                curs.execute(f"UPDATE {db_table} SET name = %s WHERE name = %s AND userid = %s", (edit_dish["name"], dish["name"], dish["userid"]))
+            if edit_dish["dish"]:
+                curs.execute(f"UPDATE {db_table} SET dish = %s WHERE dish = %s AND userid = %s", (edit_dish["dish"], dish["dish"], dish["userid"]))
 
             if edit_dish["components"]:
                 # Convert components key to JSON
                 list_to_json_components = [json.dumps(component) for component in edit_dish["components"]]
-                curs.execute(f"UPDATE {db_table} SET components = %s WHERE name = %s AND userid = %s", (list_to_json_components, edit_dish["name"], dish["userid"]))
+                curs.execute(f"UPDATE {db_table} SET components = %s WHERE dish = %s AND userid = %s", (list_to_json_components, edit_dish["dish"], dish["userid"]))
 
             # For tests
-            curs.execute(f"SELECT name, components, userid FROM {db_table}")
+            curs.execute(f"SELECT dish, components, userid FROM {db_table}")
             res = dict(curs.fetchone())
             # Decode JSON data
             res["components"] = [json.loads(component) for component in res["components"]]
-            print(res)
 
     return res
 
