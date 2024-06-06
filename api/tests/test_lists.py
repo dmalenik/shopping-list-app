34235927cf646164cb55a2c8c4e5c3325f49ed8a
Dmytro <dmalenik@gmail.com>
2024-06-06 15:38:01 +0300

    test(test_lists): create test case for a function that edits current list entry in db

--- api/tests/test_lists.py
+++ api/tests/test_lists.py
@@ -2,7 +2,7 @@ import os
 import sys
 
 sys.path.append(os.path.abspath("/shopping-list-app/api/"))
-from lists import create_list
+from lists import create_list, edit_list
 
 
 class TestLists:
@@ -17,9 +17,23 @@ class TestLists:
         "userid": 59
     }
 
+    edit_list = {
+        "list": "edit",
+        "elements": [
+            {"name": "component111", "unit": "kg", "size": 211},
+            {"name": "component222", "unit": "gr", "size": 322},
+            {"name": "component333", "unit": "cm", "size": 433},
+        ],
+        "userid": 59
+    }
+
 
     # Test cases
     def test_create_list(self):
         assert create_list(self.list) == self.list
+
+    
+    def test_edit_list(self):
+        assert edit_list(self.list, self.edit_list) == self.edit_list
 
 
