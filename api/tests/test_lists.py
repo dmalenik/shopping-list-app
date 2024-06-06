3fdaa5c74625b5552428b6b3b4ffc50d181b7e43
Dmytro <dmalenik@gmail.com>
2024-06-06 16:29:08 +0300

    test(test_lists): create test case for delete_list function

--- api/tests/test_lists.py
+++ api/tests/test_lists.py
@@ -2,7 +2,7 @@ import os
 import sys
 
 sys.path.append(os.path.abspath("/shopping-list-app/api/"))
-from lists import create_list, edit_list
+from lists import create_list, edit_list, delete_list
 
 
 class TestLists:
@@ -31,9 +31,10 @@ class TestLists:
     # Test cases
     def test_create_list(self):
         assert create_list(self.list) == self.list
-
     
     def test_edit_list(self):
         assert edit_list(self.list, self.edit_list) == self.edit_list
 
+    def test_delete_list(self):
+        assert delete_list(self.edit_list) == self.edit_list
 
