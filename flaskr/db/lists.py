1498c59a2959a61b9b12daea6b9d3308f5431589
Dmytro <dmalenik@gmail.com>
2024-06-10 17:03:03 +0300

    refactor(lists): change list key name to name

--- flaskr/db/lists.py
+++ flaskr/db/lists.py
@@ -5,6 +5,7 @@ import psycopg2.extras
 import json
 
 from dotenv import load_dotenv
+
 from dishes import add_dish
 
 load_dotenv()
@@ -18,10 +19,10 @@ def create_list(list):
         with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as curs:
             # Prepare dictionaries to be placed in db
             list_to_string_elements = [json.dumps(element) for element in list["elements"]]
-            curs.execute(f"INSERT INTO {db_table}(list, elements, userid) VALUES(%s, %s, %s);", (list["list"], list_to_string_elements, list["userid"]))
+            curs.execute(f"INSERT INTO {db_table}(name, elements, userid) VALUES(%s, %s, %s);", (list["name"], list_to_string_elements, list["userid"]))
 
             # For tests
-            curs.execute(f"SELECT list, elements, userid FROM {db_table} WHERE userid = %s AND list = %s;", (list["userid"], list["list"]))
+            curs.execute(f"SELECT name, elements, userid FROM {db_table} WHERE userid = %s AND name = %s;", (list["userid"], list["name"]))
             res = dict(curs.fetchone())
             # Decode JSON
             res["elements"] = [json.loads(element) for element in res["elements"]]
@@ -29,21 +30,21 @@ def create_list(list):
     return res
 
 
-def edit_list(list, edit_list):
+def edit_list(list, list_edit):
     with psycopg2.connect(db) as conn:
         with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as curs:
             # Edit list name
-            if edit_list["list"]:
-                curs.execute(f"UPDATE {db_table} SET list = %s WHERE list = %s AND userid = %s;", (edit_list["list"], list["list"], list["userid"]))
+            if list_edit["name"]:
+                curs.execute(f"UPDATE {db_table} SET name = %s WHERE name = %s AND userid = %s;", (list_edit["name"], list["name"], list["userid"]))
             
             # Edit list elements
-            if edit_list["elements"]:
-                list_to_string_elements = [json.dumps(element) for element in edit_list["elements"]]
-                curs.execute(f"UPDATE {db_table} SET elements = %s WHERE list = %s AND userid = %s;", (list_to_string_elements, edit_list["list"], edit_list["userid"]))
+            if list_edit["elements"]:
+                list_to_string_elements = [json.dumps(element) for element in list_edit["elements"]]
+                curs.execute(f"UPDATE {db_table} SET elements = %s WHERE name = %s AND userid = %s;", (list_to_string_elements, list_edit["name"], list_edit["userid"]))
             
 
             # For tests
-            curs.execute(f"SELECT list, elements, userid FROM {db_table} WHERE list = %s AND userid = %s;", (edit_list["list"], edit_list["userid"]))
+            curs.execute(f"SELECT name, elements, userid FROM {db_table} WHERE name = %s AND userid = %s;", (list_edit["name"], list_edit["userid"]))
             res = dict(curs.fetchone())
             res["elements"] = [json.loads(element) for element in res["elements"]]
 
@@ -53,7 +54,7 @@ def edit_list(list, edit_list):
 def delete_list(list):
     with psycopg2.connect(db) as conn:
         with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as curs:
-            curs.execute(f"DELETE FROM {db_table} WHERE list = %s AND userid = %s RETURNING list, elements, userid;", (list["list"], list["userid"]))
+            curs.execute(f"DELETE FROM {db_table} WHERE name = %s AND userid = %s RETURNING name, elements, userid;", (list["name"], list["userid"]))
 
             # For tests
             res = dict(curs.fetchone())
