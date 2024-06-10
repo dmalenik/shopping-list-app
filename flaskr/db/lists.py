7a11c1218041764079c1d64c96a80805007454ce
Dmytro <dmalenik@gmail.com>
2024-06-10 17:00:43 +0300

    feat(lists): implement feature to add ingidients to shopping list

--- flaskr/db/lists.py
+++ flaskr/db/lists.py
@@ -5,6 +5,7 @@ import psycopg2.extras
 import json
 
 from dotenv import load_dotenv
+from dishes import add_dish
 
 load_dotenv()
 
@@ -61,4 +62,42 @@ def delete_list(list):
 
     return res
 
+def add_dish_to_list(dish, list):
+    # For tests
+    # Insert some data into dishes table
+    add_dish(dish)
+    # Insert some data into lists table
+    create_list(list)
+    with psycopg2.connect(db) as conn:
+        with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as curs:    
+            # Update shopping list with dish elements
+            # Get shopping list of some user
+            curs.execute(f"SELECT elements FROM {db_table} WHERE name = %s AND userid = %s;", (list["name"], list["userid"]))
+            res = dict(curs.fetchone())
+            res["elements"] = [json.loads(element) for element in res["elements"]]
+            print(res)
+
+            # Append elements from dish to shopping list
+            for component in dish["components"]:
+                for element in res["elements"]:
+                    # If some of dish elements are already present
+                    if component["name"] == element["name"]:
+                        # Then update only their size
+                        element["size"] += component["size"]
+                    else:
+                        res["elements"].append(component)
+                    
+                    break
+            
+            # Convert elements list to json again
+            res["elements"] = [json.dumps(element) for element in res["elements"]]
+            # Insert updated elements list back to db
+            curs.execute(f"UPDATE {db_table} SET elements = %s WHERE name = %s AND userid = %s;", (res["elements"], list["name"], list["userid"]))
+
+            # For tests
+            curs.execute(f"SELECT * FROM {db_table} WHERE name = %s AND userid = %s;", (list["name"], list["userid"]))
+            res = dict(curs.fetchone())
+            res["elements"] = [json.loads(element) for element in res["elements"]]
+
+    return res["elements"]
 
