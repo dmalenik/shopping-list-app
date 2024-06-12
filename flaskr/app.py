045c2954b72e82dad9cd73c1fa5440ab8293a34c
Dmytro <dmalenik@gmail.com>
2024-06-12 20:44:25 +0300

    feat(app): create register view

--- flaskr/app.py
+++ flaskr/app.py
@@ -26,3 +26,31 @@ app.config["PERMANENT_SESSION_LIFETIME"] = timedelta(seconds=60 )
 app.config["SESSION_USE_SIGNER"] = True
 
 Session(app)
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        credentials = {
+            "username": request.form["username"],
+            "email": request.form["email"],
+            "password": request.form["password"]
+        }
+
+        if register_credentials_valid(credentials):
+            register(credentials)
+
+            return redirect(url_for("login"))
+        
+        else:
+            return redirect(url_for("error", type="index"))
+
+    # Is a temporary solution for front-end on GET method
+    return '''
+        <form action="/" method="post">
+            <input name="username" placeholder="Name"/>
+            <input name="email" placeholder="Email"/>
+            <input type="password" name="password" placeholder="password"/>
+            <button type="submit">Submit</button>
+        </form>
+    '''
+
+
