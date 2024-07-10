81451fefac447fb1a35a41584b6844ea74564a70
Dmytro <dmalenik@gmail.com>
2024-07-10 17:35:32 +0300

    refactor(get_user_data): remove email field from response on query

--- api/db/users.py
+++ api/db/users.py
@@ -24,7 +24,7 @@ def register_user(user):
 def get_user_data(credentials):
     with psycopg2.connect(db) as conn:
         with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as curs:
-            curs.execute(f"SELECT id, username FROM {db_table} WHERE username = %s;", (credentials["username"],))
+            curs.execute(f"SELECT id, username, email FROM {db_table} WHERE username = %s;", (credentials["username"],))
 
             res = curs.fetchone()
             if res:
