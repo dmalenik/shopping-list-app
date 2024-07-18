2f8cd6a6ff5dfce0fa910a22954bb55b29bb391a
Dmytro <dmalenik@gmail.com>
2024-07-18 17:00:10 +0300

    refactor: improve Dishes component
    - add state to hold updates data
    - add navigate to navigate to update route
    - handle negative values for loaderData
    - add type to loaderData parameters
    - pass updates down to child routes

--- client/src/routes/dishes/Dishes.tsx
+++ client/src/routes/dishes/Dishes.tsx
@@ -1,37 +1,57 @@
-import React from 'react';
-import {Link, Outlet, useLoaderData} from 'react-router-dom';
+import React, {useState} from 'react';
+import {Link, Outlet, useLoaderData, useNavigate} from 'react-router-dom';
 
 export const Dishes = () => {
-  const loaderData = useLoaderData();
+  const loaderData: any = useLoaderData();
+  const [updates, setUpdates] = useState();
+  const navigate = useNavigate();
+
+  const handleUpdates = up => {
+    setUpdates(up);
+    navigate('update');
+  };
 
   return (
     <div>
-      {!loaderData.success ? (
-        loaderData.map(({id, dish, components}, i) => (
-          <div key={i}>
-            <div>{dish}</div>
-            {components.map((component, idx) => {
-              const obj = JSON.parse(component);
-              const {name, unit, size} = obj;
-
-              return (
-                <div key={idx}>
-                  <div>{name}</div>
-                  <div>{unit}</div>
-                  <div>{size}</div>
-                </div>
-              );
-            })}
-            <div>{id}</div>
-            <button type="button">Update</button>
-          </div>
-        ))
-      ) : (
+      {!loaderData || loaderData.success ? (
         <div>No dishes in the list</div>
+      ) : (
+        loaderData.map(
+          (
+            obj: {
+              dish: string;
+              components: {name: string; unit: string; size: string}[];
+              id: string;
+            },
+            i: number
+          ) => {
+            const {dish, components, id} = obj;
+
+            return (
+              <div key={i}>
+                <div>{dish}</div>
+                {components.map((component, idx) => {
+                  const {name, unit, size} = component;
+
+                  return (
+                    <div key={idx}>
+                      <div>{name}</div>
+                      <div>{unit}</div>
+                      <div>{size}</div>
+                    </div>
+                  );
+                })}
+                <div>{id}</div>
+                <button type="button" onClick={() => handleUpdates(obj)}>
+                  Update
+                </button>
+              </div>
+            );
+          }
+        )
       )}
       <Link to={'add'}>Add new dish</Link>
-      <Link to={'update'}>Update dish</Link>
-      <Outlet />
+      <Outlet context={[updates, setUpdates]} />
     </div>
   );
 };
