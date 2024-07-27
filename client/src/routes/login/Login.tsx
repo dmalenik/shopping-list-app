8a4dd8c0294c10d54476cdeb7599aba89f0fccf8
Dmytro <dmalenik@gmail.com>
2024-07-27 13:35:39 +0300

    feat(Login): apply styles

--- client/src/routes/login/Login.tsx
+++ client/src/routes/login/Login.tsx
@@ -1,7 +1,56 @@
 import React, {useEffect} from 'react';
 import {useActionData, useNavigate, Form, Link} from 'react-router-dom';
+import {styled} from 'styled-components';
 
 import {useLoginState} from '../../hooks';
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 2%;
+`;
+
+const Navigation = styled.nav`
+  align-self: flex-end;
+  margin-top: 1%;
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  font-size: 1.1rem;
+`;
+
+const Menu = styled.div`
+  display: flex;
+  column-gap: 10px;
+`;
+
+const LoginLink = styled(Link)<{$isActive?: boolean}>`
+  color: ${props => (props.$isActive ? 'red' : 'inherit')};
+`;
+
+const Header = styled.header`
+  align-self: center;
+  margin-top: 3%;
+`;
+
+const LoginForm = styled(Form)`
+  margin-top: 1%;
+  align-self: center;
+  display: flex;
+  flex-direction: column;
+  row-gap: 20px;
+  width: 20%;
+`;
+
+const FormController = styled.div`
+  display: flex;
+  flex-direction: column;
+  row-gap: 5px;
+`;
+
+const FormButton = styled.button`
+  margin-top: 15px;
+`;
 
 export const Login = () => {
   const actionData = useActionData();
@@ -16,14 +65,30 @@ export const Login = () => {
   }, [actionData]);
 
   return (
-    <div className="login">
-      <Link to={'/'}>Home</Link>
-      Hello from Login
-      <Form method="post">
-        <input name="username" placeholder="Enter name" />
-        <input type="password" name="password" placeholder="Enter password" />
-        <button type="submit">Log in</button>
-      </Form>
-    </div>
+    <Container>
+      <Navigation>
+        <Link to={'/'}>Home</Link>
+        <Menu>
+          <Link to={'../register'}>Register</Link>
+          <LoginLink to={'.'} $isActive>
+            Login
+          </LoginLink>
+        </Menu>
+      </Navigation>
+      <Header>
+        <h2>Login!</h2>
+      </Header>
+      <LoginForm method="post">
+        <FormController>
+          <label htmlFor="username">Username</label>
+          <input name="username" placeholder="Your name" />
+        </FormController>
+        <FormController>
+          <label htmlFor="password">Password</label>
+          <input type="password" name="password" placeholder="Your password" />
+        </FormController>
+        <FormButton type="submit">Login</FormButton>
+      </LoginForm>
+    </Container>
   );
 };
