ed825860e51384174ef7b8ecbe6b2769ee233730
Dmytro <dmalenik@gmail.com>
2024-07-27 13:34:55 +0300

    feat(Home): apply styles

--- client/src/routes/home/Home.tsx
+++ client/src/routes/home/Home.tsx
@@ -1,13 +1,59 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import {styled} from 'styled-components';
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 2%;
+`;
+
+const Navigation = styled.nav`
+  align-self: flex-end;
+  margin-top: 1%;
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  font-size: 1.1rem;
+`;
+
+const HomeLink = styled(Link)<{$isActive?: boolean}>`
+  color: ${props => (props.$isActive ? 'red' : 'inherit')};
+`;
+
+const Menu = styled.div`
+  display: flex;
+  column-gap: 10px;
+`;
+
+const Header = styled.header`
+  align-self: center;
+  margin-top: 3%;
+`;
+
+const Main = styled.main`
+  align-self: center;
+  margin-top: 9%;
+`;
 
 export const Home = () => {
   return (
-    <div>
-      <Link to={'register'}>Register here</Link>
-      <Link to={'login'}>Login here</Link>
-      <p>Home is where you are!</p>
-      <p>Plan your shopping here</p>
-    </div>
+    <Container>
+      <Navigation>
+        <HomeLink to={'./'} $isActive>
+          Home
+        </HomeLink>
+        <Menu>
+          <Link to={'register'}>Register</Link>
+          <Link to={'login'}>Login</Link>
+        </Menu>
+      </Navigation>
+      <Header>
+        <h1>Home page!</h1>
+      </Header>
+      <Main>
+        <p>Plan your shoppings here</p>
+      </Main>
+    </Container>
   );
 };
