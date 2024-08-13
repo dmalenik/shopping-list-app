9e5d4b7cfe6c20fff9ee82aefc5258356d06bbbf
Dmytro <dmalenik@gmail.com>
2024-08-13 18:47:35 +0300

    feat(config.py): comment out alternative permanent session lifetime option

--- api/config.py
+++ api/config.py
@@ -8,6 +8,7 @@ SESSION_TYPE = "filesystem"
 
 # User remains logged in for 1 hour
 PERMANENT_SESSION_LIFETIME = timedelta(hours=1)
+# PERMANENT_SESSION_LIFETIME = timedelta(minutes=1)
 
 # Use it to cryptographically-sign cookies
 SESSION_USE_SIGNER = True
